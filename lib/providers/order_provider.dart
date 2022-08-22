fde90f20cda611fd2206589fc556978bb6259392
MdTamjidIslam <mdtamjidislam2001@gmail.com>
2022-08-22 17:24:31 +0600

    Initial commit 01

---
+++ lib/providers/order_provider.dart
@@ -0,0 +1,27 @@
+
+import 'package:admin_e_com/db/dbhelper.dart';
+import 'package:admin_e_com/models/order_constants_model.dart';
+import 'package:flutter/material.dart';
+
+class OrderProvider extends ChangeNotifier {
+  OrderConstantsModel orderConstantsModel = OrderConstantsModel();
+
+  getOrderConstants() {
+    DbHelper.getOrderConstants().listen((event) {
+      if(event.exists) {
+        orderConstantsModel = OrderConstantsModel.fromMap(event.data()!);
+        notifyListeners();
+      }
+    });
+  }
+
+  Future<void> getOrderConstants2() async {
+    final snapshot = await DbHelper.getOrderConstants2();
+    orderConstantsModel = OrderConstantsModel.fromMap(snapshot.data()!);
+    notifyListeners();
+  }
+
+  Future<void> addOrderConstants(OrderConstantsModel model) =>
+      DbHelper.addOrderConstants(model);
+
+}
